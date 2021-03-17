82380844910aaec2791667b77758161a0aaa5ac8
Decilap denis <decilapdenis@gmail.com>
2021-03-17 11:38:18 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -3,43 +3,43 @@ let cookie = document.querySelector(".footer_cookie")
 let cookieClose = document.querySelector(".popclose")
 
 console.log(element)
-element.addEventListener ('click', event => {
-    console.log(event);
- cookie.classList.toggle("hide")   
+// element.addEventListener ('click', event => {
+//     console.log(event);
+//  cookie.classList.toggle("hide")   
     
-})
+// })
 
-cookieClose.addEventListener ('click', event => {
-    console.log(event);
-    cookie.style.display='none';
+// cookieClose.addEventListener ('click', event => {
+//     console.log(event);
+//     cookie.style.display='none';
     
-})
+// })
 
-let plusOuMoin = 0
+// let plusOuMoin = 0
 
-function afficherSousCat(event){
-    //document.querySelector('.contenu').classList.add('show')
-    let i2 = document.querySelector('.fa-plus')
-    console.log(i2)
-    plusOuMoin++
-    let isActive = document.querySelector('.fas.fa-minus')
-    if (plusOuMoin % 2 ==0){
-        i2.classList.remove('fa-plus')
-        i2.classList.add('fa-minus')
-    } else {
-        i2.classList.remove('fa-minus')
-        i2.classList.add('fa-plus')
-    }
-}
+// function afficherSousCat(event){
+//     //document.querySelector('.contenu').classList.add('show')
+//     let i2 = document.querySelector('.fa-plus')
+//     console.log(i2)
+//     plusOuMoin++
+//     let isActive = document.querySelector('.fas.fa-minus')
+//     if (plusOuMoin % 2 ==0){
+//         i2.classList.remove('fa-plus')
+//         i2.classList.add('fa-minus')
+//     } else {
+//         i2.classList.remove('fa-minus')
+//         i2.classList.add('fa-plus')
+//     }
+// }
 
-listeCat = [".categorie_meilleurs_ventes", ".categorie_tendances_hommes", ".categorie_tendances_femmes", ".categorie_tendances"]
+// listeCat = [".categorie_meilleurs_ventes", ".categorie_tendances_hommes", ".categorie_tendances_femmes", ".categorie_tendances"]
 
 
-//let titlesAccordeon = document.querySelectorAll(".accordeon div")
-for (let i=0; i < listeCat.length; i++){
-    console.log(i);
-    document.querySelector(listeCat[i]).addEventListener('click',afficherSousCat)
-}
+// //let titlesAccordeon = document.querySelectorAll(".accordeon div")
+// for (let i=0; i < listeCat.length; i++){
+//     console.log(i);
+//     document.querySelector(listeCat[i]).addEventListener('click',afficherSousCat)
+// }
 
 function Menu() {
     let self = this;
